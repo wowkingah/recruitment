6834c5e01f439b9e2cf0e1a5990ef2b56bebad84
Wowking <wowkingah@gmail.com>
2022-01-23 17:34:59 +0800

    add interview

--- interview/admin.py
+++ interview/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from interview.models import Candidate
 
+
 # Register your models here.
 
 
@@ -17,15 +18,17 @@ class CandidateAdmin(admin.ModelAdmin):
         (None, {'fields': ("userid", ("username", "city", "phone"), ("email", "apply_position", "born_address"),
                            ("gender", "candidate_remark"), ("bachelor_school", "master_school", "doctor_school"),
                            ("major", "degree"), ("test_score_of_general_ability", "paper_score"), "last_editor",)}),
-        ('第一轮面试记录', {'fields': (("first_score", "first_learning_ability", "first_advantage"), "first_disadvantage",
-                                "first_result", "first_recommend_position", "first_interviewer", "first_remark",)}),
-        ('第二轮专业复试记录', {'fields': ("second_score", "second_learning_ability", "second_professional_competency",
-                                "second_pursue_of_excellence", "second_communication_ability", "second_pressure_score",
-                                "second_advantage", "second_disadvantage", "second_result", "second_recommend_position",
-                                "second_interviewer", "second_remark",)}),
-        ('HR复试记录', {'fields': ("hr_score", "hr_responsibility", "hr_communication_ability", "hr_logic_ability",
-                                "hr_potential", "hr_stability", "hr_advantage", "hr_disadvantage", "hr_result",
-                                "hr_interviewer", "hr_remark",)}),
+        ('第一轮面试记录', {'fields': (("first_score", "first_learning_ability", "first_professional_competency"),
+                                "first_advantage", "first_disadvantage", "first_result", "first_recommend_position",
+                                "first_interviewer", "first_remark",)}),
+        ('第二轮专业复试记录', {'fields': (("second_score", "second_learning_ability", "second_professional_competency"),
+                                  ("second_pursue_of_excellence", "second_communication_ability",
+                                  "second_pressure_score"), "second_advantage", "second_disadvantage", "second_result",
+                                  "second_recommend_position", "second_interviewer", "second_remark",)}),
+        ('HR复试记录', {'fields': ("hr_score", ("hr_responsibility", "hr_communication_ability", "hr_logic_ability"),
+                               ("hr_potential", "hr_stability"), "hr_advantage", "hr_disadvantage", "hr_result",
+                               "hr_interviewer", "hr_remark",)}),
     )
+
 
 admin.site.register(Candidate, CandidateAdmin)
