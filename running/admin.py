3513898655a6a4e631c3305fc1c1964267ea54cd
Wowkingah <wowkingah@gmail.com>
2022-11-12 20:53:39 +0800

    实现只读站点ReadOnlyAdmin

--- running/admin.py
+++ running/admin.py
@@ -3,20 +3,41 @@ from .models import Country, Province, Area, City
 
 
 # Register your models here.
+class ReadOnlyAdmin(admin.ModelAdmin):
+    readonly_fields = []
+
+    def get_readonly_fields(self, request, obj=None):
+        return list(self.readonly_fields) + \
+               [field.name for field in obj._meta.fields] + \
+               [field.name for field in obj._meta.many_to_many]
+
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.concrete_fields]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
 @admin.register(Country)
-class CountryAdmin(admin.ModelAdmin):
+class CountryAdmin(ReadOnlyAdmin):
     search_fields = ('chn_name', 'eng_name')
 
 
 @admin.register(Province)
-class ProvinceAdmin(admin.ModelAdmin):
+class ProvinceAdmin(ReadOnlyAdmin):
     search_fields = ('chn_name', 'eng_name')
 
 
-class CityAdmin(admin.ModelAdmin):
+class CityAdmin(ReadOnlyAdmin):
     # 定义自动完成的字段
     autocomplete_fields = ['provinceid', 'countryid']
-    list_display = ('cityid', 'countryid', 'areaid', 'provinceid', 'chn_name', 'eng_name')
+    # list_display = ('cityid', 'countryid', 'areaid', 'provinceid', 'chn_name', 'eng_name')
 
 
 admin.site.register(City, CityAdmin)
